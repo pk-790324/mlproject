9d2c4556849dce71ba9400e80fb0451d12fd9ea9
pk-790324 <pappuyadav98199@gmail.com>
2025-09-21 20:36:08 +0545

    second commit

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import find_packages,setup
+from typing import List
+hypen_e='-e .'
+def get_requirements(file_path:str)->List[str]:
+    #this function will return list of requirements
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        requirements=[require.replace('/n','') for require in requirements]
+
+        if hypen_e in requirements:
+            requirements.remove(hypen_e)
+
+    return requirements
+
+setup(
+    name='mlproject',
+    version='0.0.1',
+    author='pk-790324',
+    author_email='pappuyadav98199@gmail.com',
+    packages=find_packages(),
+    install_requires=get_requirements('requirements.txt'),
+)
